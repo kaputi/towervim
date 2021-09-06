d95a7932bb34a1b9d0d84d59d86d3e0659b52eef
Eduardo Lonighi <edu_loni3@hotmail.com>
2021-09-06 06:41:03 +0100

    nvimtree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,17 @@ return {
   -- Packwe can manage itself as a plugin
   {'wbthomason/packer.nvim'},
 
+
+  -- ====================
+  -- Syntax, LSP, Autocomplete
+  -- ====================
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = [[require('plugin-settings.treesitter')]]
+  },
+
   -- ====================
   -- CODE
   -- ====================
@@ -32,7 +43,34 @@ return {
   -- Tabbar, statusbar
   {
     'romgrk/barbar.nvim',
-    config = [[require('plugin-settings.barbar')]]
-  }
+    config = [[require('plugin-settings.barbar')]],
+    event = 'BufWinEnter'
+  },
+  {
+    'glepnir/galaxyline.nvim',
+    config = [[require('plugin-settings.galaxyline')]],
+    event = 'BufWinEnter'
+  },
+
+  -- ====================
+  -- Tools
+  -- ====================
+
+  -- whichkey
+  {
+    'folke/which-key.nvim',
+    config = [[require('plugin-settings.whichkey')]]
+  },
+
+  -- File tree
+  {
+    'kyazdani42/nvim-tree.lua',
+    config = [[require('plugin-settings.nvimtree')]]
+  },
+   
+  -- fuzzy  find
+  {'nvim-lua/popup.nvim'},
+  {'nvim-lua/plenary.nvim'},
+  {'nvim-telescope/telescope.nvim'},
 
 }
