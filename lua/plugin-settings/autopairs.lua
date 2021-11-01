38286d097621b75f576d96c225e91a0f65327792
Eduardo Lonighi <edu_loni3@hotmail.com>
2021-11-01 15:33:08 +0000

    mapped <CR> for autopairs

--- lua/plugin-settings/autopairs.lua
+++ lua/plugin-settings/autopairs.lua
@@ -1,4 +1,6 @@
 local ok, nvim_autopairs = pcall(require, 'nvim-autopairs')
+local cmp_ok, cmp = pcall(require, 'cmp')
+
 if ok then
   nvim_autopairs.setup({
     disable_filetype = { 'TelescopePrompt' },
@@ -8,4 +10,12 @@ if ok then
     enable_check_bracket_line = true, --- check bracket in same line
     check_ts = false,
   })
+  if cmp_ok then
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done({ map_char = { tex = '' } })
+    )
+  end
 end
