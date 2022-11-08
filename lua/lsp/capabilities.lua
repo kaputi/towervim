81fb26a899cce3fe5b95c95995c1a0763cd617e2
Eduardo Lonighi <edu_loni3@hotmail.com>
2022-11-08 09:50:11 +0000

    wip

--- lua/lsp/capabilities.lua
+++ lua/lsp/capabilities.lua
@@ -1,10 +1,9 @@
 local function getCapabilities()
-
   local capabilities = vim.lsp.protocol.make_client_capabilities()
 
   local cmp_ok, cmp = pcall(require, 'cmp_nvim_lsp')
   if cmp_ok then
-    capabilities = cmp.update_capabilities(capabilities)
+    capabilities = cmp.default_capabilities(capabilities)
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     capabilities.textDocument.completion.completionItem.resolveSupport = {
       properties = { 'documentation', 'detail', 'additionalTextEdits' },
