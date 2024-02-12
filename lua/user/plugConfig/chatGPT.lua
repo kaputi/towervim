f0b1e1480c0d0f718f24c6b2b796bb8c170d78e4
Eduardo Lonighi <edu_loni3@hotmail.com>
2024-02-12 08:55:32 +0000

    gpt-4

--- lua/user/plugConfig/chatGPT.lua
+++ lua/user/plugConfig/chatGPT.lua
@@ -22,9 +22,14 @@ chatgpt.setup({
     answer_sign = 'ﮧ', -- 🤖
   },
   openai_params = {
+    model = 'gpt-4-turbo-preview',
     max_tokens = 2000,
+  },
+  openai_edit_params = {
+    -- model = 'gpt-4-0125-preview',
+    -- model = 'gpt-4',
+    model = 'gpt-4-turbo-preview',
   },
   show_quickfixes_cmd = 'Trouble quickfix',
   predefined_chat_gpt_prompts = 'https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv',
 })
-
